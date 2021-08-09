fea9cfca6e6394b1ff2b8b442401eb34abf3737b
pacifico3000 <62159316+pacifico3000@users.noreply.github.com>
2021-08-10 02:26:43 +0300

    algo WIP

--- algo.php
+++ algo.php
@@ -1,1 +1,26 @@
 <?php
+
+$input = [[1,6], [3,8], [5,7], [6,12]];
+
+echo getMaxIntersections($input);
+echo PHP_EOL . memory_get_peak_usage();
+
+function getMaxIntersections(array $periods) {
+    $sub1 = $sub2 = $max = $i = 0;
+    foreach ($periods as $first) {
+        if ($i > $max) {
+            $max = $i;
+        }
+        $i = 0;
+        foreach ($periods as $second) {
+            $x1 = $first[0];
+            $x2 = $first[1];
+            $x3 = $second[0];
+            $x4 = $second[1];
+            if () {
+                $i++;
+            }
+        }
+    }
+    return $max;
+}
